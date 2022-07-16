ffbd23563e728b70dcca62223c26a260f8e8ba0e
zakirovravshan <zakirovrovshan@gmail.com>
2022-07-16 11:45:06 +0500

    final add

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var elBoxbutton = document.querySelector(".button");
+var elBoxcontent = document.querySelector(".checklist");
+
+elBoxbutton.addEventListener("click" , function () {
+    elBoxcontent.classList.toggle("open")
+})
+
+var elBoxbutton1 = document.querySelector(".button--two");
+var elBoxcontent1 = document.querySelector(".checklist--two");
+
+elBoxbutton1.addEventListener("click" , function () {
+    elBoxcontent1.classList.toggle("open")
+})
+
+var elBoxbutton2 = document.querySelector(".button--three");
+var elBoxcontent2 = document.querySelector(".checklist--three");
+
+elBoxbutton2.addEventListener("click" , function () {
+    elBoxcontent2.classList.toggle("open")
+})
+
+var elBoxbutton3 = document.querySelector(".button--four");
+var elBoxcontent3 = document.querySelector(".checklist--four");
+
+elBoxbutton3.addEventListener("click" , function () {
+    elBoxcontent3.classList.toggle("open")
+})
+
+var elBoxbutton4 = document.querySelector(".button--five");
+var elBoxcontent4 = document.querySelector(".checklist--five");
+
+elBoxbutton4.addEventListener("click" , function () {
+    elBoxcontent4.classList.toggle("open")
+})
